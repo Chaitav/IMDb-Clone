da61e0b79f7034e24d240f142dd34f7ab1212e92
Chaitav Shah <chaitavshah69@gmail.com>
2023-08-02 11:38:20 +0530

    Created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,43 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl text-center font-medium text-amber-600">About</h1>
+      <h2 className="text-xl font-medium">Introduction</h2>
+      <p>
+        Welcome to our movie review website, your ultimate destination for the
+        latest and most comprehensive reviews of both current and classic
+        movies. Here at our website, we strive to provide an impartial and
+        honest assessment of films from various genres and eras. Whether you are
+        a film buff or a casual viewer, we have got you covered with our
+        in-depth analysis and diverse range of reviews.
+      </p>
+      <h2 className="text-xl font-medium">Our Mission</h2>
+      <p>
+        Our mission is to help movie enthusiasts make informed decisions about
+        the films they watch. We understand the significance of a good
+        movie-watching experience and believe that it all begins with selecting
+        the right movie. Our team of expert reviewers is dedicated to watching
+        and analyzing movies, allowing us to offer you accurate and insightful
+        evaluations.
+      </p>
+      <h2 className="text-xl font-medium">What We Offer</h2>
+      <p>
+        Objective and Subjective Reviews: We aim to strike the perfect balance
+        between objective analysis and subjective opinions. Our reviews provide
+        objective information about various aspects of a film, including the
+        plot, cinematography, directing, acting, and editing. At the same time,
+        we also incorporate our personal perspectives, giving you an honest and
+        relatable account of our viewing experience.
+      </p>
+      <p>
+        Featured Articles and Editorials: In addition to movie reviews, we bring
+        you a range of engaging articles and editorials. Our team of writers
+        delves deeper into various film-related topics, exploring the latest
+        trends, discussing different genres, and spotlighting notable actors and
+        directors. These articles aim to enhance your movie-watching experience
+        by providing additional insights and sparking thought-provoking
+        discussions.
+      </p>
+    </div>
+  );
+}
